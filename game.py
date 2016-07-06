3ba50d43bc29848b05f447fbc183d2569a54200f
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 12:51:44 -0700

    Basic number guessing

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import random
+
 # Greet player
 print "Hello there! Welcome to the guessing game!"
 # Get name
@@ -5,6 +7,22 @@ name = raw_input("What's your name? ")
 print "Hi {}!".format(name)
 
 #picking a random number
-import random
 secret_number = random.randint(1, 100)
-print secret_number
+#print secret_number
+
+solved = False
+
+while solved == False:
+    guess = int(raw_input("Pick a number: "))
+
+    if guess > secret_number:
+        print "Too high"
+
+    elif guess < secret_number:
+        print "Too low"
+
+    else:
+        print "You got it!"
+        solved = True
+
+
