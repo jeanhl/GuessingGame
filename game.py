3f3fbfd8b523869671b4a1947cae00108a044408
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 12:33:09 -0700

    Picks random number

--- game.py
+++ game.py
@@ -3,3 +3,8 @@ print "Hello there! Welcome to the guessing game!"
 # Get name
 name = raw_input("What's your name? ")
 print "Hi {}!".format(name)
+
+#picking a random number
+import random
+secret_number = random.randint(1, 100)
+print secret_number
