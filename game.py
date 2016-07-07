ce06d8b0e4acdbe34cd045764ed577963fecba87
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 17:50:27 -0700

    high score is now a param not a global var, also min max are params

--- game.py
+++ game.py
@@ -1,12 +1,10 @@
 import random
 
-HIGH_SCORE = 101
-
-def guessing_game(HIGH_SCORE):
+def guessing_game(min_number,max_number,high_score='max_number+1'):
     '''Plays a guessing game'''
 
     #picking a random number
-    secret_number = random.randint(1, 100)
+    secret_number = random.randint(min_number, max_number)
     print "The secret number is:",secret_number
 
     solved = False
@@ -15,7 +13,7 @@ def guessing_game(HIGH_SCORE):
     #this will keep track of number of guesses
 
     while not solved:
-        guess = raw_input("Pick a number between 1 and 100: ")
+        guess = raw_input("Pick a number between {} and {}: ".format(min_number, max_number))
         # keeps asking for guesses while not solved
 
         try:
@@ -28,8 +26,8 @@ def guessing_game(HIGH_SCORE):
         guesses.add(guess)
         # adds new guess to set of guessed numbers; does not add duplicates
 
-        if guess > 100 or guess < 1:
-            print "I said between 1 and 100."
+        if guess > max_number or guess < min_number:
+            print "I said between {} and {}.".format(min_number, max_number)
 
         elif guess > secret_number:
             print "Too high."
@@ -39,16 +37,15 @@ def guessing_game(HIGH_SCORE):
 
         else:
             print "You got it! You guessed {} numbers.".format(len(guesses))
-            # now we will track the high score. 
-            # HIGH SCORE is a global variable because it will reset if stored in the function
-            if len(guesses) < HIGH_SCORE:
-                HIGH_SCORE = len(guesses)
-                print "{} is the least amount of numberers guessed!".format(HIGH_SCORE)
+            # now we will track the high score
+            if len(guesses) < high_score:
+                high_score = len(guesses)
+                print "{} is the least amount of numberers guessed!".format(high_score)
             solved = True
     # Asking player if they want to play again 
     again = raw_input("Do you want to play again? (Y/N) ")
     if again == "Y" or again == "y":
-        guessing_game(HIGH_SCORE)
+        guessing_game(min_number,max_number)
     else:
         return
 
@@ -58,7 +55,7 @@ print "Hello there! Welcome to the guessing game!"
 # name = raw_input("What's your name? ")
 # print "Hi {}!".format(name)
 
-guessing_game(HIGH_SCORE)
+guessing_game(1,100)
 
 print "Thank you for playing."
 
